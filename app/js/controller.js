3ce89914cf6502bbe1a817a023fc1777904e1a8d
Lise Poirier <lise.p.poirier@gmail.com>
2016-10-21 17:29:06 +0200

    Ajout de dettes

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,58 +1,3 @@
-// résumé des créances
-/*app.controller("resumController", function ($scope) {
-    $scope.resums = [
-        {
-         "id":"273",
-         "montant":"23€",         
-         "payeur":"Exemple payeur",
-         "concernes":"Exemple user1, user2, user3",
-         "date":"",
-         "nbConcernes":"3",
-         "usergroup":"Exemple Group",
-         "description":"Exemple description / Users 1 : 2.50 euros pour des feuilles de tabac. Users 2 : 7.50 euros pour un paquet de blonde. Users3 : 10 euros pour tabac + feuille."
-        }
-
-  ]
-});
-
-app.controller("resumFormController", function ($scope) {
-    $scope.addUser = function (user) {
-        $scope.resums.push(user);
-        $scope.resum = {};
-    };
-
-});*/
-
-
-
-//controller d'ajout de débiteurs
-/*
-app.controller("userController", function ($scope) {
-    $scope.users = [
-        {
-        }
-      ]
-});*/
-
-
-
-/*
-app.controller('userFormController', function($scope, $http) {
-  $http.get("users.json").then(function (response) {
-      $scope.myData = response.data.records;
-  });
-});
-
-app.controller("userFormController", function ($scope) {
-    $scope.addUser = function (user) {
-        $scope.users.push(user);
-        $scope.user = {};
-    };
-
-});*/
-
-
-
 app.controller('usersCtrl', function($scope, $http) {
   $http({
       method:'GET',
@@ -74,12 +19,3 @@ app.controller("userFormController", function ($scope) {
     };
 
 });
-
-/*app.controller("userSup", function ($scope) {
-    $scope.supUser = function (sup) {
-        var sup = angular.element('{{users.Id}}');
-        $scope.usersData.splice(sup);
-        $scope.usersData = {};
-    };
-
-});*/
--- app/js/script.js
+++ app/js/script.js
@@ -2,41 +2,77 @@
 var btnAddUsers = document.getElementById('btnAddUsers');
 var modal = document.getElementById('myModal');
 var span = document.getElementById('close1');
-var submit = document.getElementById("submit");
+var submit = document.getElementById('submit');
 
-btnAddUsers.onclick = function() {
+btnAddUsers.onclick = function () {
     modal.style.display = "block";
 }
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
 }
-submit.onclick = function() {
+submit.onclick = function () {
     modal.style.display = "none";
 }
-
 
 //MODAL GROUPE
-var btnAddGroups = document.getElementById("btnAddGroups");
+var btnAddGroups = document.getElementById('btnAddGroups');
 var modal2 = document.getElementById('myModal2');
 var span2 = document.getElementById('close2');
-var submit2 = document.getElementById("submit2");
+var submit2 = document.getElementById('submit2');
 
-btnAddGroups.onclick = function() {
+btnAddGroups.onclick = function () {
     modal2.style.display = "block";
 }
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal2) {
         modal2.style.display = "none";
     }
 }
-span2.onclick = function() {
+span2.onclick = function () {
     modal2.style.display = "none";
 }
-submit2.onclick = function() {
+submit2.onclick = function () {
     modal2.style.display = "none";
 }
+
+
+
+//SELECT BOX USERS
+$(".dropdown dt a").on('click', function() {
+  $(".dropdown dd ul").slideToggle('fast');
+});
+
+$(".dropdown dd ul li a").on('click', function() {
+  $(".dropdown dd ul").hide();
+});
+
+function getSelectedValue(id) {
+  return $("#" + id).find("dt a span.value").html();
+}
+
+$(document).bind('click', function(e) {
+  var $clicked = $(e.target);
+  if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
+});
+
+$('.mutliSelect input[type="checkbox"]').on('click', function() {
+
+  var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
+    title = $(this).val() + ",";
+
+  if ($(this).is(':checked')) {
+    var html = '<span title="' + title + '">' + title + '</span>';
+    $('.multiSel').append(html);
+    $(".hida").hide();
+  } else {
+    $('span[title="' + title + '"]').remove();
+    var ret = $(".hida");
+    $('.dropdown dt a').append(ret);
+
+  }
+});

c6b25a835ca3ea7e60a33f20eaca7c71420e98bd
Lise Poirier <lise.p.poirier@gmail.com>
2016-10-24 17:30:50 +0200

    Mise en place des couleurs + dettes + users

--- app/js/controller.js
+++ app/js/controller.js
@@ -4,9 +4,13 @@ app.controller('usersCtrl', function($scope, $http) {
       url : 'js/users.json'
   }).then(function (response) {
       $scope.usersData = response.data.records;
-      
-  } , 
-         function errorCallback(response){
+        $scope.aUsers = [];
+        //Generating array aUsers with structure [id => username]
+        for($user in $scope.usersData){
+            $scope.aUsers[$scope.usersData[$user].Id] = $scope.usersData[$user];
+        }
+  }, 
+        function errorCallback(response){
       console.log("Failure");
       console.error(response);
   });
@@ -16,6 +20,30 @@ app.controller("userFormController", function ($scope) {
     $scope.addUser = function (user) {
         $scope.usersData.push(user);
         $scope.user = {};
+    };
+
+});
+
+
+app.controller('usersDepenses', function($scope, $http) {
+  $http({
+      method:'GET',
+      url : 'js/depenses.json'
+  }).then(function (response) {
+      $scope.depensesData = response.data.records;
+      console.log($scope.aUsers);
+      
+  } , 
+         function errorCallback(response){
+      console.log("Failure");
+      console.error(response);
+  });
+});
+
+app.controller("userFormController2", function ($scope) {
+    $scope.addDepense = function (depense) {
+        $scope.depensesData.push(depense);
+        $scope.depense = {};
     };
 
 });

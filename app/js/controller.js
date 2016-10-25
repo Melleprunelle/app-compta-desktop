8153e905331e455af82a625c31b816e8e88859ea
Lise Poirier <lise.p.poirier@gmail.com>
2016-10-25 12:11:25 +0200

    Finition de l'interface graphique

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,49 +1,53 @@
-app.controller('usersCtrl', function($scope, $http) {
-  $http({
-      method:'GET',
-      url : 'js/users.json'
-  }).then(function (response) {
-      $scope.usersData = response.data.records;
-        $scope.aUsers = [];
-        //Generating array aUsers with structure [id => username]
-        for($user in $scope.usersData){
-            $scope.aUsers[$scope.usersData[$user].Id] = $scope.usersData[$user];
-        }
-  }, 
-        function errorCallback(response){
-      console.log("Failure");
-      console.error(response);
-  });
-});
+app.controller('usersCtrl', function ($scope, $http) {
+            $http({
+                method: 'GET',
+                url: 'js/users.json'
+            }).then(function (response) {
+                    $scope.usersData = response.data.records;
+                    $scope.aUsers = [];
+                    //Generating array aUsers with structure [id => username]
+                    for ($user in $scope.usersData) {
+                        $scope.aUsers[$scope.usersData[$user].Id] = $scope.usersData[$user];
+                    }
+                    
+                        function errorCallback(response) {
+                            console.log("Failure");
+                            console.error(response);
+                        }});
+            });
 
-app.controller("userFormController", function ($scope) {
-    $scope.addUser = function (user) {
-        $scope.usersData.push(user);
-        $scope.user = {};
-    };
+        app.controller("userFormController", function ($scope) {
+            $scope.addUser = function (user) {
+                $scope.usersData.push(user);
+                $scope.user = {};
+                
+                $scope.selection = [];
+                $scope.Selection = function Selection(usersUsersname) {
+                        $scope.selection.push(usersUsersname);
+                        }
+            };
+        });
 
-});
 
+        app.controller('usersDepenses', function ($scope, $http) {
+            $http({
+                method: 'GET',
+                url: 'js/depenses.json'
+            }).then(function (response) {
+                    $scope.depensesData = response.data.records;
+                    console.log($scope.aUsers);
 
-app.controller('usersDepenses', function($scope, $http) {
-  $http({
-      method:'GET',
-      url : 'js/depenses.json'
-  }).then(function (response) {
-      $scope.depensesData = response.data.records;
-      console.log($scope.aUsers);
-      
-  } , 
-         function errorCallback(response){
-      console.log("Failure");
-      console.error(response);
-  });
-});
+                },
+                function errorCallback(response) {
+                    console.log("Failure");
+                    console.error(response);
+                });
+        });
 
-app.controller("userFormController2", function ($scope) {
-    $scope.addDepense = function (depense) {
-        $scope.depensesData.push(depense);
-        $scope.depense = {};
-    };
+        app.controller("userFormController2", function ($scope) {
+            $scope.addDepense = function (depense) {
+                $scope.depensesData.push(depense);
+                $scope.depense = {};
+            };
 
-});
+        });
